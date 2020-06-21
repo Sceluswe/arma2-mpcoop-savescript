e770fa586f0e35dbac893df8f0fb08613213e2fd
Sceluswe <emilcbm@protonmail.com>
2020-06-21 18:25:07 +0200

    add functionality to remove autosaves & continue saves if any are present

--- savescript.sh
+++ savescript.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# Remove all autosaves.
+rm -rf *autosave*
+
+# Remove all continue saves.
+rm -rf *continue*
+
 #If backup directory does not exist create it.
 FILE=backup
 if [[ ! -d "$FILE" ]]; then
 	mkdir backup;
 fi
 
+# Loop through the save files and create backup folders.
+# Name each folder after the creation date of the save file.
