08b2fe1882e06483576c37bf5b0f88a460d01231
Sceluswe <emilcbm@protonmail.com>
2020-06-21 22:42:40 +0200

    create folders using savegames modification date

--- savescript.sh
+++ savescript.sh
@@ -12,6 +12,24 @@ if [[ ! -d "$FILE" ]]; then
 	mkdir backup;
 fi
 
-# Every minute loop through the save files and create backup folders.
+# Save the two campaign files.
+#cp campaign.sqc backup/campaign.sqc
+#cp objects.sav backup/objects.sav
+
+# Loop through the save files and create backup folders.
+for i in mpsave*; do # Whitespace-safe but not recursive.
+	SAVEDATE=$(stat -c %y $i)
+	SAVEDATE=${SAVEDATE%.*}
+	SAVEDATE=${SAVEDATE// /_}
+	SAVEDATE=${SAVEDATE//:/.}
+	mkdir backup/$SAVEDATE
+	# Save the two campaign files.
+	# cp campaign.sqc backup/campaign.sqc
+	# cp objects.sav backup/objects.sav
+	echo $SAVEDATE
+done
+
+
 
 # Name each folder after the creation date of the save file.
+
