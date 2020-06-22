32a8248bba97b5a64ae65137ab20179fb832808f
Sceluswe <emilcbm@protonmail.com>
2020-06-22 02:04:03 +0200

    rename script and add capaign and object files to backup folders

--- arma2savescript.sh
+++ arma2savescript.sh
@@ -17,19 +17,19 @@ fi
 #cp objects.sav backup/objects.sav
 
 # Loop through the save files and create backup folders.
-for i in mpsave*; do # Whitespace-safe but not recursive.
+for i in save*; do
+	# Create a folder named after the save files original modification date.
 	SAVEDATE=$(stat -c %y $i)
 	SAVEDATE=${SAVEDATE%.*}
 	SAVEDATE=${SAVEDATE// /_}
 	SAVEDATE=${SAVEDATE//:/.}
 	mkdir backup/$SAVEDATE
-	# Save the two campaign files.
-	# cp campaign.sqc backup/campaign.sqc
-	# cp objects.sav backup/objects.sav
+	
+	# Save the two campaign files and the two related save files in the folder.
+	cp -p campaign.sqc backup/$SAVEDATE/campaign.sqc
+	cp -p objects.sav backup/$SAVEDATE/objects.sav
+	#mv $i.* backup/$SAVEDATE/$i.*
+	#mv mp$i.* backup/$SAVEDATE/mp$i.*
 	echo $SAVEDATE
 done
-
-
-
-# Name each folder after the creation date of the save file.
 
