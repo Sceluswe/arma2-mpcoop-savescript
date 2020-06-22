e61254df949c7b276f22a98ab139b554b76c20cd
Sceluswe <emilcbm@protonmail.com>
2020-06-23 00:49:29 +0200

    remove old comments

--- arma2savescript.sh
+++ arma2savescript.sh
@@ -6,15 +6,11 @@ rm -rf *autosave*
 # Remove all continue saves.
 rm -rf *continue*
 
-#If backup directory does not exist create it.
+#If the backup directory does not exist, create it.
 FILE=backup
 if [[ ! -d "$FILE" ]]; then
 	mkdir backup;
 fi
-
-# Save the two campaign files.
-#cp campaign.sqc backup/campaign.sqc
-#cp objects.sav backup/objects.sav
 
 # Loop through the save files and create backup folders.
 for FILENAME in save*; do
@@ -28,8 +24,7 @@ for FILENAME in save*; do
 	# Save the two campaign files and the two related save files in the folder.
 	cp -p campaign.sqc backup/$SAVEDATE/campaign.sqc
 	cp -p objects.sav backup/$SAVEDATE/objects.sav
-	cp -p $FILENAME backup/$SAVEDATE/$FILENAME
+	mv $FILENAME backup/$SAVEDATE/$FILENAME
 	SAVENAME=${FILENAME%%.*}
-	cp -p --parent "mp$SAVENAME."* backup/$SAVEDATE/
-	# echo mp$SAVENAME.*
+	mv "mp$SAVENAME."* backup/$SAVEDATE/
 done
